55ff411f5a3d1a6116277af5196a355fddcff9aa
Drollie123 <152189626+Drollie123@users.noreply.github.com>
2024-01-22 16:06:06 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,45 @@
+let noorden = 0
+basic.forever(function () {
+    if (noorden < 315 && noorden > 225) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+})
+basic.forever(function () {
+    if (noorden < 225 && noorden > 135) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+})
+basic.forever(function () {
+    if (noorden > 315 && noorden < 45) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+})
+basic.forever(function () {
+    if (noorden > 45 && noorden < 135) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+})
